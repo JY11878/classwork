d3d53d6a8b79a26f0cefc1f436de1497b4bb375a
JY <113320951+JY11878@users.noreply.github.com>
2022-09-26 13:06:54 -0400

    Add files via upload

---
+++ java_study/Java_logic/src/main/java/com/mycompany/java_logic/Java_logic.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.java_logic;
+
+/**
+ *
+ * @author Augustus
+ */
+public class Java_logic {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int ans1 = a + b;
+        int ans2 = a - b;
+        
+        System.out.println((ans1>0) && (ans2>0));
+        System.out.println((ans1>0) || (ans2>0));
+        System.out.println(!(ans1>0));
+        
+    }
+}
