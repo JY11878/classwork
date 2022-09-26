d0bf1aebfeb68c9b1d39620913b210633e8f6c72
JY <113320951+JY11878@users.noreply.github.com>
2022-09-26 13:03:57 -0400

    Add files via upload

---
+++ java_study/study_p1/src/main/java/com/mycompany/study_p1/Study_p1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.study_p1;
+
+/**
+ *
+ * @author Augustus
+ */
+public class Study_p1 {
+   
+    public static void main(String[] args) {
+        int length = 10;
+        int width = 20;
+        int area = length*width;
+         System.out.println("area="+area);
+    }
+}
